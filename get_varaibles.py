2b37ec108cda1471d8ffa6ae82e0502c0f344a54
Daniel Ellis <daniel.ellis@ext.esa.int>
2023-10-24 00:38:15 +0100

    Duplication free merging.

--- get_varaibles.py
+++ get_varaibles.py
@@ -1,8 +1,12 @@
 import json
 import logging
 import os
+import copy
 from pathlib import Path
 from multiprocessing import Pool
+
+from collections import defaultdict
+
 
 def process_file(filepath):
     """
@@ -16,23 +20,29 @@ def process_file(filepath):
     """
     with open(filepath) as f:
         data = json.load(f)
-    
+
     variables = {}
     for var in data['variable_entry'].values():
         try:
-            name = (var['out_name'], var['branded_variable_name'])
+            name = var['out_name']
+            # (var['out_name'], var['branded_variable_name'])
         except Exception as e:
             # Log and skip entries with missing or unexpected keys
             logging.warning(f"Error processing variable in {filepath}: {e}")
             continue
 
         if name not in variables:
-            variables[tuple(name)] = var
+            variables[name] = var
         else:
             # Log conflict for duplicate variables
             logging.warning(f"Variable conflict: {name}")
-    
+
     return variables
+
+
+def clean(d, key_to_ignore):
+    return {key: d[key] for key in d if key not in key_to_ignore}
+
 
 def compare_dict(d1, d2, key_to_ignore):
     """
@@ -41,17 +51,22 @@ def compare_dict(d1, d2, key_to_ignore):
     Args:
         d1 (dict): First dictionary.
         d2 (dict): Second dictionary.
-        key_to_ignore (str): Key to ignore during the comparison.
+        key_to_ignore (list): Keys to ignore during the comparison.
 
     Returns:
         bool: True if the dictionaries are equal ignoring the specified key, False otherwise.
     """
+    assert isinstance(key_to_ignore, list)
+
     # Create copies of the dictionaries without the ignored key
-    filtered_d1 = {key: d1[key] for key in d1 if key != key_to_ignore}
-    filtered_d2 = {key: d2[key] for key in d2 if key != key_to_ignore}
+    filtered_d1 = clean(d1, key_to_ignore)
+    filtered_d2 = clean(d2, key_to_ignore)
+
+    # print('\n\n',filtered_d1.keys(),'\n', filtered_d2.keys())
 
     # Compare the filtered dictionaries
     return filtered_d1 == filtered_d2
+
 
 def nest(dictionary_with_tuples):
     """
@@ -72,6 +87,7 @@ def nest(dictionary_with_tuples):
             nested_dict[keys[0]] = new
     return nested_dict
 
+
 if __name__ == '__main__':
     # Find all JSON files in the 'Tables' directory
     files = Path('./Tables').glob('*.json')
@@ -87,23 +103,56 @@ if __name__ == '__main__':
     with Pool() as pool:
         results = pool.map(process_file, files)
 
-    merged = {}
+    merged = defaultdict(dict)
     for result in results:
         for name, var in result.items():
+
+            pv = copy.deepcopy(var['provenance'])
+
+            # if 'provenance' not in merged[name]:
+            #     merged[name]['provenance'] = defaultdict(dict)
+
+            for MIP in pv:
+                if MIP not in var:
+                    var['provenance'][MIP] = []
+
+                entry = pv[MIP]
+                for key in ['frequency', 'branded_variable_name', "modeling_realm"]:
+                    entry[key] = var[key]
+                del entry['variable_name']
+
+                var['provenance'][MIP].append(dict(sorted(entry.items())))
+
             if name in merged:
                 # Check for conflicts in variable information, specifically in 'provenance' key
-                if compare_dict(merged[name], var, 'provenance'):
-                    merged[name]['provenance'].append(var.get('provenance'))
+
+                if compare_dict(merged[name], var, ['provenance', 'frequency', 'branded_variable_name', "modeling_realm", 'cell_measures', 'cell_methods', 'dimensions']):
+                    # dreq uid in provenances
+                    # merged[name]['provenance'].append(var.get('provenance'))
+                    for MIP in var['provenance']:
+                        merged[name]['provenance'][MIP].append(
+                            var['provenance'][MIP])
+
+                        merged[name]['dimensions'].append(
+                            tuple(var['dimensions']))
+
+                        merged[name]['dimensions'] = list(
+                            set(merged[name]['dimensions']))
+
                 else:
                     # Log conflicts in variable information
-                    logging.warning(f"\n\nConflict for ** {name} **: \n{var} \n!= \n{merged[name]}") 
+                    logging.warning(
+                        f"\n\nConflict for ** {name} **: \n{var} \n!= \n{merged[name]}")
             else:
-                var['provenance'] = [var['provenance']]
-                merged[name] = var
 
-    # Sort merged dictionary by key 
+                merged[name] = clean(var, ['frequency', 'branded_variable_name',
+                                     "modeling_realm", 'cell_measures', 'cell_methods'])
+                merged[name]['dimensions'] = [
+                    tuple(merged[name]['dimensions'])]
+
+    # Sort merged dictionary by key
     merged = dict(sorted(merged.items()))
 
     # Save the nested and merged variable information to a JSON file
     with open('Tables/Auxillary/variables.json', 'w') as f:
-        json.dump(nest(merged), f, indent=2)
+        json.dump(merged, f, indent=2)
