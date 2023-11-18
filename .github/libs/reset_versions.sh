27a750d404c256e6722d465bd7a9217d4c1f0596
CMIP-IPO: Automated GitHub Action <actions@wcrp-cmip.org>
2023-11-18 00:31:09 +0000

    rm duplication in versioning

---
+++ .github/libs/reset_versions.sh
@@ -0,0 +1,14 @@
+json_files=$(grep -rl '"version_metadata"' *.json)
+
+# Iterate over each JSON file and update the 'checksum' element
+for json_file in $json_files; do
+  jq '.version_metadata.checksum = "reset"' "$json_file" > "$json_file.tmp" && mv "$json_file.tmp" "$json_file"
+done
+
+
+json_files=$(grep -rl '"version_metadata"' Auxillary*/*.json)
+
+# Iterate over each JSON file and update the 'checksum' element
+for json_file in $json_files; do
+  jq '.version_metadata.checksum = "reset"' "$json_file" > "$json_file.tmp" && mv "$json_file.tmp" "$json_file"
+done
