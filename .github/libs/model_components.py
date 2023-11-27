25b5173db7b8e3db276ae9f7820186ba89505900
Dan Ellis <daniel.ellis@ext.esa.int>
2023-11-27 20:48:56 +0000

    Reading Airtable model_components into a file.

---
+++ .github/libs/model_components.py
@@ -0,0 +1,66 @@
+import urllib.request
+import json
+from collections import OrderedDict
+import sys
+
+base_id,table_name,view_name = 'appaZflpqbFjA6pwV/tblD5m3Bxsph5VjZ0/viwxN1LyTlEA2TZ5W'.split('/')
+
+# read from action
+api_key = sys.argv[1]  # Replace with your actual Airtable API key
+
+url = f'https://api.airtable.com/v0/{base_id}/{table_name}?view={view_name}'
+
+# Set up headers with your Airtable API key
+headers = {
+    'Authorization': f'Bearer {api_key}',
+    'Content-Type': 'application/json',
+}
+
+# Create a request with headers
+request = urllib.request.Request(url, headers=headers)
+
+
+models = {}
+
+# Make the API request to get data in JSON format
+with urllib.request.urlopen(request) as response:
+    # Process the response as needed
+    if response.status == 200:
+        data = json.loads(response.read().decode('utf-8'))
+        records = data.get('records', [])
+        for record in records:
+            fields = record.get('fields', {})
+            realm = fields['realm']
+            description = fields['description']
+            resolution = fields['resolution']+' km'
+            if realm not in models: 
+                models[realm] = {}
+            if description not in models[realm]:
+                models[realm][description] = {'description' : description, 'native_nominal_resolutions':[]}
+
+            if resolution not in models[realm][description]['native_nominal_resolutions']:
+                models[realm][description]['native_nominal_resolutions'].append(resolution)
+           
+
+
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status}")
+
+
+
+def sort_dict_recursive(input_dict):
+    if isinstance(input_dict, dict):
+        return OrderedDict((key, sort_dict_recursive(value)) for key, value in sorted(input_dict.items()))
+    elif isinstance(input_dict, list):
+        return [sort_dict_recursive(item) for item in input_dict]
+    else:
+        return input_dict
+    
+
+models = sort_dict_recursive(models)
+
+file_path = 'output.json'
+
+# Write the dictionary to the JSON file
+with open('Auxillary_files/MIP_model_components.json', 'w') as json_file:
+    json.dump(models, json_file, indent=2)
