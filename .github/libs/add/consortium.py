c40fe638fdd19dfb7389d9921b8b214b7486346c
Dan Ellis <daniel.ellis.research@gmail.com>
2024-04-19 14:37:42 +0100

    Update consortium.py

--- .github/libs/add/consortium.py
+++ .github/libs/add/consortium.py
@@ -23,19 +23,73 @@ issue_title = os.environ.get('ISSUE_TITLE')
 issue_body = os.environ.get('ISSUE_BODY')
 issue_submitter = os.environ.get('ISSUE_SUBMITTER')
 
-print(os.popen('github.event.issue.body').read())
-
-
 print(issue_body, issue_submitter)
 
-# Define regular expressions to extract the values
-name_regex = r"\*\*Name\*\*\n(.*?)\n"
-part_of_regex = r"\*\*Who is this part of \*\*\n(.*?)\n"
 
-# Extract values using regular expressions
-consortium_name = re.search(name_regex, issue_body, re.DOTALL).group(1).strip()
-part_of = re.search(part_of_regex, issue_body, re.DOTALL).group(1).strip()
+def parse_md(body):
 
-print("Consortium Name:", consortium_name)
-print("Part of:", part_of)
-print(issue_body, issue_submitter)
+
+      # remove comments
+      pattern = r'<!---(.*?)--->'
+
+      # Remove comments using re.sub
+      body = re.sub(pattern, '', body, flags=re.DOTALL)
+
+  
+      template_data = {}
+
+      # Read lines from the file
+      lines = [i for i in body.split('\n') if i.strip()[0] !=  '#']
+
+      # Iterate over each line
+      current_key = None
+      for line in lines:
+          # Strip leading and trailing whitespaces
+          line = line.strip()
+
+          # Skip empty lines and lines starting with '##' (section headings)
+          if not line or line.startswith("##"):
+              continue
+
+          # Check for lines containing '**'
+          if line.startswith("**") and ":" in line:
+              # Extract key and value
+              key, value = line.split(":", 1)
+              key = key.strip("**").strip()
+              value = value.strip()
+              
+              # Store the current key-value pair
+              template_data[key] = value
+              current_key = key
+          else:
+              # If it's a continuation of the previous line, append it to the value
+              if current_key:
+                  template_data[current_key] += " " + line
+
+    return template_data
+
+
+
+
+parsed = parse_md(issue_body)
+
+
+
+
+
+
+# # Define regular expressions to extract the values
+# name_regex = r"\*\*Name\*\*\n(.*?)\n"
+# part_of_regex = r"\*\*Who is this part of \*\*\n(.*?)\n"
+
+
+
+# # # Extract values using regular expressions
+# consortium_name = re.search(name_regex, issue_body, re.DOTALL).group(1).strip()
+# part_of = re.search(part_of_regex, issue_body, re.DOTALL).group(1).strip()
+
+# print("Consortium Name:", consortium_name)
+# print("Part of:", part_of)
+# print(issue_body, issue_submitter)
+
+
