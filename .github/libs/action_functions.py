acef7db551d1b3795d5ba7f562a21211d9e52a96
Daniel <daniel.ellis@ext.esa.int>
2024-04-20 22:42:42 +0100

    rename

--- .github/libs/action_functions.py
+++ .github/libs/action_functions.py
@@ -83,7 +83,7 @@ def jr(file):
     return json.load(open(file,'r'))
 
 def jw(file,data):
-    return json.dumps(data,open(file,'w'), indent=4)
+    return json.dump(data,open(file,'w'), indent=4)
 
 def getfile(fileend):
     import glob
