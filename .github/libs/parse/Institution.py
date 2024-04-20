8d42b171aef35dfcc9f70e312661ddd3fa9d89b7
Daniel <daniel.ellis@ext.esa.int>
2024-04-20 23:22:04 +0100

    test

--- .github/libs/parse/Institution.py
+++ .github/libs/parse/Institution.py
@@ -136,7 +136,8 @@ institutions['institutions'] = ilist
 # Serialize back to JSON
 jw(institutions, getfile('institutions')[0])
 
-os.popen(f'git commit -a "New entry {data["acronym"]} to the Institutions file"').read()
+os.popen(f'git add -A"').read()
+os.popen(f'git commit -m "New entry {data["acronym"]} to the Institutions file"').read()
 
 
 
