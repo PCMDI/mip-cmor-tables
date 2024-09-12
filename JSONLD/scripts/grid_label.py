28bc0700edf98971132be187cc17ce6babc95c9a
Daniel Ellis <daniel.ellis@ext.esa.int>
2024-09-12 11:41:19 +0100

    generate grid

---
+++ JSONLD/scripts/grid_label.py
@@ -0,0 +1,20 @@
+from __setup__ import * 
+
+lddata = cmipld.sync(lddata)
+
+name = 'grid_label'
+
+frame = {
+        "@type": [
+            f"mip:{name.replace('_','-')}", 
+        ],
+        "@embed":"@always",
+}
+
+
+frame = cmipld.Frame(lddata,frame).clean(['rmld','untag'])
+
+data = cmipld.utils.sorted_dict(frame.keystr('name','description'))
+
+
+cmipld.utils.wjsn(finalise(data,name),base+f'/MIP_{name}.json')
