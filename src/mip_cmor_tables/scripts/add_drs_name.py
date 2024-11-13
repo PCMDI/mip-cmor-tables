f92e43e7ec43131571b484cf5cb397c4bc9cfdbe
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2024-11-13 09:51:37 +0100

    little function do help load and save

--- src/mip_cmor_tables/scripts/add_drs_name.py
+++ src/mip_cmor_tables/scripts/add_drs_name.py
@@ -4,14 +4,22 @@ import json
 
 base_dir = Path("datadescriptor")
 
+def load_data(term_path:Path):
+    with open(term_path,"r") as f:
+        data = json.load(f)
+    return data
+
+def save_data(data:dict, term_path:Path):
+    with open(term_path,"w") as f:
+        json.dump(data, f, indent=4)
+
 activity_dir = Path("activity")
+
 for term_path in (base_dir / activity_dir).iterdir():
     # print(term_path)
     if term_path.suffix==".json":
         print(term_path)
-        with open(term_path,"r") as f:
-            data = json.load(f)
+        data = load_data(term_path)
         data["drs_name"] = data["cmip_acronym"]
-        with open(term_path,"w") as f:
-            json.dump(data, f, indent=4)
-            
+        save_data(data,term_path)
+                    
